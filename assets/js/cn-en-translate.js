0a5260c6cf55887eb5e6f3e10a4a25788976742d
YUKI-JR <xjy1252@163.com>
2023-07-07 14:04:46 +0800

    修改i18n文件夹路劲

--- assets/js/cn-en-translate.js
+++ assets/js/cn-en-translate.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var defaultLang = "en";
     $("[i18n]").i18n({
         defaultLang: defaultLang,
-        filePath: "/assets/i18n/",//路径配置
+        filePath: "./i18n/",//路径配置
         filePrefix: "i18n_",
         fileSuffix: "",
         forever: true,
@@ -14,14 +14,13 @@ $(document).ready(function () {
     /*中英文切换按钮*/
     $("#translate").click(function (e) {
         var a = $(e.target).val() == "cn" ? "en" : "cn";
-        var b = a=="cn"?"中/En":"En/中";
+        var b = a == "cn" ? "中/En" : "En/中";
         $(e.target).val(a);
         $("#nav__translate").text(b);
         console.log($(e.target).val());
-
         $("[i18n]").i18n({
             defaultLang: a,
-            filePath: "/assets/i18n/",
+            filePath: "./i18n/",
         });
 
     });
